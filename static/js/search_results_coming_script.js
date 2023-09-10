8c272c8cbbf59a80de1fdcb3d8c1688319afc877
MohamadAlgazli <brawjym@gmail.com>
2023-09-10 18:24:39 +0300

    fixing coming category

--- static/js/search_results_coming_script.js
+++ static/js/search_results_coming_script.js
@@ -1,6 +1,5 @@
 const determingAddingComingForm = document.getElementById("determing-adding-coming-form");
 const comingPointsInput = determingAddingComingForm.querySelector("#coming-points");
-const comingCategoryInput = determingAddingComingForm.querySelector("#coming-category");
 const submitComingButtons = document.querySelectorAll(".submit-coming-button");
 const studentIdHiddenInput = document.getElementById("student-id-hidden-input");
 const warningModelComing = document.getElementById("warning-model-coming");
@@ -9,20 +8,14 @@ if (localStorage.getItem("coming-points")) {
     comingPointsInput.value = localStorage.getItem("coming-points");
 }
 
-if (localStorage.getItem("coming-category")) {
-    comingCategoryInput.value = localStorage.getItem("coming-category");
-}
-
 determingAddingComingForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const determingAddingModel = document.querySelector(".model.active");
 
     const comingPoints = comingPointsInput.value;
-    const comingCategory = comingCategoryInput.value;
 
     localStorage.setItem("coming-points", comingPoints);
-    localStorage.setItem("coming-category", comingCategory);
 
     closeModel(determingAddingModel);
 });
