6a3f92dabaa7576a135f02bc1d0790b8344169cb
MohamadAlgazli <brawjym@gmail.com>
2023-09-15 15:20:40 +0300

    adding dotenv.read_dotenv in settings.py

--- main_project/settings.py
+++ main_project/settings.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 import os
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+dotenv.read_dotenv(BASE_DIR, ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
